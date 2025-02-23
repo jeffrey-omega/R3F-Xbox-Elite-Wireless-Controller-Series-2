34cd94607d4dc48a2a89e547d689dfbffbd3ccb1
jeffrey-omega <1844253+jeffrey-omega@users.noreply.github.com>
2025-02-23 19:24:06 +0100

    fix: enhance logging for environment variables in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import "@/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-console.log(process.env.ENV);
+console.log("env", process.env.ENV);
+console.log("node env", process.env.NODE_ENV);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
